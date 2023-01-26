8645481abd66def220ceaf866fcfb60dd3abfe22
Brad <52509751+BRadHoc@users.noreply.github.com>
2023-01-26 15:56:17 +0000

    🗄 Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # bdsx-sleep
+![logo](logo-sml.png)
+
 A 50% / 1 Player Sleep script for @BDSX/bdsx
+
+## Configuration
+
+Change:
+
+```js
+let OnePlayerSleep = true;
+```
+
+to enable One Player Sleep, otherwise it's default 50% of the players online
+
+## Usage
+* Requirements
+    * [bdsx](https://github.com/bdsx/bdsx) Please make sure your node is up to date.
+
+To download, clone the repo to your `bdsx/plugins` directory:
+```bash
+git clone https://github.com/BRadHoc/bdsx-sleep.git bdsx-sleep
+```
+
+### How to use?
+Just go to bed! This script hooks into the default behaviour for sleeping in minecraft and replaces the functionality to enable 50% or one player to sleep for everyone.
---
+++ index.js
@@ -0,0 +1,58 @@
+/**
+ * 50% Player Sleep Plugin
+ * Discord: @Brad (BDSX)
+ */
+const { bedrockServer } = require("bdsx/launcher");
+const { events } = require("bdsx/event");
+const { ServerPlayer } = require("bdsx");
+const { procHacker } = require("bdsx/bds/proc");
+const { void_t, bool_t } = require("bdsx/nativetype");
+const command = require("bdsx/commandresult");
+
+let OnePlayerSleep = false;
+
+let sleepRemind = true;
+let wakeUp = procHacker.js('?stopSleepInBed@ServerPlayer@@UEAAX_N0@Z', void_t, {this: ServerPlayer}, bool_t, bool_t);
+events.playerSleepInBed.on((ev) => {
+    setTimeout(() => {
+        if (ev.player.isSleeping()) {
+
+            const players = bedrockServer.level.getPlayers();
+            const sleepingPlayers = players.filter((player) => player.isSleeping()).length;
+
+            if (sleepRemind) {
+                sleepRemind = false;
+                if (!OnePlayerSleep) {
+                    bedrockServer.executeCommand(`tellraw @a {"rawtext":[{"text":"§bThere are ${sleepingPlayers}/${players.length} players in-bed."}]}`, () => { });
+                    bedrockServer.executeCommand('tellraw @a {"rawtext":[{"text":"§b' + (!OnePlayerSleep ? "50% of the players online are required to sleep." : ev.player.getNameTag().toString() + " just slept for everyone...") + '"}]}', () => { });
+                }
+                setTimeout(() => { sleepRemind = true; }, 30000);
+            }
+
+            setTimeout(() => {
+                if (sleepingPlayers >= players.length / 2 || OnePlayerSleep) {
+                    if (ev.player.isSleeping()) {
+                        for (let x = 0; x < players.length; x++) {
+                            if (players[x].isPlayer()) {
+                                if (players[x].isSleeping()) {
+                                    wakeUp.call(players[x], true, true);
+                                }
+                                let reset = players[x].save();
+                                reset.SleepTimer._value = 0;
+                                players[x].load(reset);
+                            }
+                        }
+
+                        const com = bedrockServer.executeCommand("time query daytime", command.CommandResultType.OutputAndData);
+                        const time = parseInt(com.data.statusMessage.substring(11)) % 24000;
+                        const t_offset = (23999-time)+1000; //Set time to 1000
+                        bedrockServer.executeCommand(`time add ${t_offset}`);
+
+                        if (OnePlayerSleep) bedrockServer.executeCommand(`tellraw @a {"rawtext":[{"text":"§b${ev.player.getNameTag()} just slept for everyone!"}]}`, () => { });
+                        bedrockServer.executeCommand(`tellraw @a {"rawtext":[{"text":"§aGood Morning!"}]}`, () => { });
+                    }
+                }
+            }, 2000);
+        }
+    }, 2000);
+});
